24e785e5c5765d8b619fb5b99a26bd6f4a6f5f80
Eric Tillberg <eric.tillberg@gmail.com>
2015-01-26 17:07:59 -0500

    Adding Intro to Ruby Exercises

---
+++ ruby_intro.rb
@@ -0,0 +1,22 @@
+puts 'Eric ' + 'Tillberg'
+
+puts 4321
+puts 4321/1000
+puts 4321 % 1000 / 100
+puts 4321 % 1000 % 100 / 10
+puts 4321 % 1000 % 100 % 10
+
+movies = {:movie1 => 1990, :movie2 => 1992, :movie3 => 1994,
+	:movie4 => 1996, :movie5 => 1998}
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+puts movies[:movie4]
+puts movies[:movie5]
+
+dates_array = [1990, 1992, 1994, 1996, 1998]
+puts dates_array[0]
+puts dates_array[1]
+puts dates_array[2]
+puts dates_array[3]
+puts dates_array[4]
